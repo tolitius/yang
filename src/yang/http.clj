02ef3f8dcd6f9908c85c5126340240165bf8fc6e
anatoly <tolitius@gmail.com>
2020-07-30 00:44:47 -0400

    http: add "successful-http-status?"

--- src/yang/http.clj
+++ src/yang/http.clj
@@ -1,8 +1,13 @@
 (ns yang.http)
 
+(defn successful-http-status? [status]
+  (when (number? status)
+    (= 2
+       (int (/ status 100)))))
+
 (defn error-check [{:keys [error status] :as resp}]
   (if (or error
-          (not= 200 status))
+          (not (successful-http-status? status)))
     {:error (-> resp
                 (update-in [:opts :headers] dissoc "Authorization") ;; strip JWT header
                 (update-in [:headers] dissoc :authorization)) ;; ^^
